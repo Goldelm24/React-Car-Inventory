e0e83f94354853ddfafb59ddf18e06a6f4a75a5c
Josias Guzman <jguzman@aoiths.org>
2023-01-23 14:46:54 -0400

    Deployment

---
+++ src/api/server.ts
@@ -0,0 +1,55 @@
+let token = '63920c5f09b72aee87f6d0c520fd0b7b69f708b3b96d1056'
+
+export const server_calls = {
+    get: async () => {
+        const response = await fetch(`https://josias-car-collection.glitch.me/api/cars`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok){
+            throw new Error('Failed to fetch data from server')
+        }
+
+        return await response.json()
+    },
+
+    create: async(data: any = {}) => {
+        const response = await fetch(`https://josias-car-collection.glitch.me/api/cars`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to Create new data on server')
+        }
+
+        return await response.json()
+    },
+    update: async (id:string, data:any = {}) => {
+        const response = await fetch(`https://josias-car-collection.glitch.me/api/cars/${id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+    },
+    delete: async(id:string) => {
+        const response = await fetch(`https://josias-car-collection.glitch.me/cars/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        })
+    }
+}
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8Sg0gaA85CBqADGuNZ9Zgk7s7IdCIn7o",
+  authDomain: "react-car-inventory-jg.firebaseapp.com",
+  projectId: "react-car-inventory-jg",
+  storageBucket: "react-car-inventory-jg.appspot.com",
+  messagingSenderId: "287665696634",
+  appId: "1:287665696634:web:1d37c5d52ca98b312af0c3"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
